e7ce5766c0395854ffc817cf2536476f3688cf69
Siyan Zhou <siyanzhou@Siyans-MacBook-Pro.local>
2018-03-26 23:55:27 -0400

    minor bug fixed related to splice()

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,17 +8,24 @@ var unsnswered;
 var questionNumber;
 
 var questionTimer = 10;
-var displayQuestionTimer;
+var displayQtimer;
+
+var firstTimeStart = true;
+var TriviaRequestReturned = false;
 
 
 
 $(document).ready(function () {
 
     triviaAPI();
-    
-    $(document).on("click", "button", function () {
-        start();
-        questionAndTimerDisplay();
+
+    $(document).on("click", "#start", function () {
+        if (TriviaRequestReturned) {
+            start();
+            questionAndTimerDisplay();
+        } else {
+            $("#start").html("Please click me again!");
+        }
 
     }).on("click", ".correctAnswer", function () {
 
@@ -36,11 +43,17 @@ $(document).ready(function () {
         setTimeout(questionAndTimerDisplay, 3000);
         cancelQuestionTimer();
 
+    }).on("click", "#restart", function () {
+        firstTimeStart = false;
+        start();
+        questionAndTimerDisplay();
+
     });
 
-  
 
-//================================================================================================================================
+
+
+    //============================================================All functions definition below==========================================================//
 
     function start() {
         correctNumber = 0;
@@ -52,8 +65,8 @@ $(document).ready(function () {
     function questionAndTimerDisplay() {
 
         if (questionNumber < triviaQuizNumber) {
-            
-            displayQuestionTimer = setInterval(questionTimerDisplay, 1000);
+
+            displayQtimer = setInterval(QtimerDisplay, 1000);
             questionDisplay(questionNumber);
 
         } else {
@@ -64,100 +77,105 @@ $(document).ready(function () {
     }
 
 
-    /////
-    
+    function triviaAPI() {
+        $.ajax({
+            url: "https://opentdb.com/api.php?type=multiple&amount=3",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            t = response.results;
+            TriviaRequestReturned = true;
+        });
+    }
 
-        function triviaAPI() {
-            $.ajax({
-                url: "https://opentdb.com/api.php?type=multiple&amount=3",
-                method: "GET"
-            }).then(function (response) {
-                console.log(response);
-                t = response.results;
-            });
+    function QtimerDisplay() {
+
+        console.log(displayQtimer, questionTimer);
+
+        if (questionTimer > 0) {
+            $("#timer").show();
+            $("#timer").html("<p>Time Remaining: " + questionTimer + " s </p>");
+            questionTimer--;
+        } else {
+            timeoutDiplay(questionNumber);
+            unsnswered++;
+            questionNumber++;
+            setTimeout(questionAndTimerDisplay, 3000);
+            cancelQuestionTimer();
         }
 
-        function questionTimerDisplay() {
-            console.log(displayQuestionTimer, questionTimer);
-            
-            if (questionTimer > 0) {
-                $("#timer").show();
-                $("#timer").html("<p>Time Remaining: " + questionTimer + " s </p>");
-                questionTimer--;
-            } else {
-                timeoutDiplay(questionNumber);
-                unsnswered++;
-                questionNumber++;
-                setTimeout(questionAndTimerDisplay, 3000);
-                cancelQuestionTimer();
-            }
+    }
 
-        }
+    function cancelQuestionTimer() {
 
-        function cancelQuestionTimer() {
+        $("#timer").hide();
+        clearInterval(displayQtimer);
+        questionTimer = 10;
 
-            $("#timer").hide();
-            clearInterval(displayQuestionTimer);
-            timeOut = true;
-            questionTimer = 10;
+    }
 
-        }
+    function questionDisplay(n) {
+        $("#mainContainer").empty();
+        $("#mainContainer").html("<h3>" + t[n].question + "</h3>");
+        $("h3").append($("<ul></ul>"));
 
-        function questionDisplay(n) {
-            $("#mainContainer").html("<h3>" + t[n].question + "</h3>");
-            $("h3").append($("<ul></ul>"));
-
+        if (firstTimeStart) {
             t[n].incorrect_answers.splice(Math.floor(Math.random() * (t[n].incorrect_answers.length + 1)), 0, t[n].correct_answer);
+        }
 
-            for (let i = 0; i < t[n].incorrect_answers.length; i++) {
-                if (t[n].incorrect_answers[i] === t[n].correct_answer) {
-                    let answerList = $("<li>");
-                    answerList.text(t[n].incorrect_answers[i]);
-                    answerList.attr("class", "correctAnswer");
-                    $("ul").append(answerList);
+        for (let i = 0; i < t[n].incorrect_answers.length; i++) {
+            if (t[n].incorrect_answers[i] === t[n].correct_answer) {
+                let answerList = $("<li>");
+                answerList.text(t[n].incorrect_answers[i]);
+                answerList.attr("class", "correctAnswer");
+                $("ul").append(answerList);
 
-                } else {
-                    let answerList = $("<li>");
-                    answerList.text(t[n].incorrect_answers[i]);
-                    answerList.attr("class", "incorrectAnswer");
-                    $("ul").append(answerList);
-                }
+            } else {
+                let answerList = $("<li>");
+                answerList.text(t[n].incorrect_answers[i]);
+                answerList.attr("class", "incorrectAnswer");
+                $("ul").append(answerList);
             }
-
         }
 
+    }
 
 
-        function timeoutDiplay(n) {
-            $("#mainContainer").html("<h3>Out of Time!</h3>")
-            $("h3").append("<p>The Correct answer is: " + t[n].correct_answer + "</p>");
-        }
 
-        function correctAnswerDisplay() {
-            $("#mainContainer").html("<h3>Corret!</h3>")
+    function timeoutDiplay(n) {
+        $("#mainContainer").empty();
+        $("#mainContainer").html("<h3>Out of Time!</h3>")
+        $("h3").append("<p>The Correct answer is: " + t[n].correct_answer + "</p>");
+    }
 
-        }
+    function correctAnswerDisplay() {
+        $("#mainContainer").empty();
+        $("#mainContainer").html("<h3>Corret!</h3>")
 
-        function incorrectAnswerDisplay(n) {
-            $("#mainContainer").html("<h3>Nope!</h3>")
-            $("h3").append("<p>The Correct answer is: " + t[n].correct_answer + "</p>");
+    }
 
-        }
+    function incorrectAnswerDisplay(n) {
+        $("#mainContainer").empty();
+        $("#mainContainer").html("<h3>Nope!</h3>")
+        $("h3").append("<p>The Correct answer is: " + t[n].correct_answer + "</p>");
 
-        function finalResultDisplay() {
-            $("#mainContainer").html("<h3>All done, here's how you did!</h3>");
-            $("h3").append("<p>Correct Answers: " + correctNumber + "</p>");
-            $("h3").append("<p>Inorrect Answers: " + incorrectNumber + "</p>");
-            $("h3").append("<p>Unsnswered: " + unsnswered + "</p>");
+    }
 
-            let restart = $("<button>");
-            restart.text("Start Over?");
-            restart.attr("id", "restart");
-            $("h3").append(restart);
+    function finalResultDisplay() {
+        $("#mainContainer").empty();
+        $("#mainContainer").html("<h3>All done, here's how you did!</h3>");
+        $("h3").append("<p>Correct Answers: " + correctNumber + "</p>");
+        $("h3").append("<p>Inorrect Answers: " + incorrectNumber + "</p>");
+        $("h3").append("<p>Unsnswered: " + unsnswered + "</p>");
 
-        }
+        let restart = $("<button>");
+        restart.text("Start Over?");
+        restart.attr("id", "restart");
+        $("h3").append(restart);
 
-    });
+    }
+
+});
 
 
 
